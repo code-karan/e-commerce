8d1d0b8188ee177f3bc46f1e5e96c63cac66c82c
karan <ksinghasane14@gmail.com>
2019-12-27 21:30:56 +0530

    Authentication completed

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
-    path('contact/', contact)
+    path('contact/', contact),
+    path('login/', login_page),
+    path('register/', register_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import *
+from django.contrib.auth.models import User, auth
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    context = {
+        'username':request.user
+    }
+    return render(request, 'homepage.html', context)
 
 def contact(request):
     contact_form = ContactForm(request.POST or None)
@@ -17,20 +21,45 @@ def contact(request):
 
     return render(request, 'contact/view.html', context)
 
-def login(request):
+
+def login_page(request):
     login_form = LoginForm(request.POST or None)
     context = {
         'form':login_form
     }
-
+    
     if login_form.is_valid():
-        print(login_form.cleaned_data)
+        username = login_form.cleaned_data.get('username')
+        password = login_form.cleaned_data.get('password')
+        user = auth.authenticate(request, username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            context['error'] = '{} login successful'.format(username)
+        else:
+            context['error'] = 'no such user'
 
     return render(request, 'auth/login.html', context)
 
-def register(request):
+
+def register_page(request):
+    register_form = RegisterForm(request.POST or None)
     context = {
-
+        'form':register_form,
     }
-
+    if register_form.is_valid():
+        username = register_form.cleaned_data.get('username')
+        email = register_form.cleaned_data.get('email')
+        password = register_form.cleaned_data.get('password')
+        password2 = register_form.cleaned_data.get('password2')
+        user = auth.authenticate(request, username = username, password = password)
+        if password2 == password:
+            # register user
+            user = auth.authenticate(request, username = username, password = password2)
+            if user is None:
+                User.objects.create_user(username, email, password2)
+                context['error'] = '{} successfully registered'.format(username)
+            else:
+                context['error'] = 'user {} already exists. Please login'.format(username)
+        else:
+            context['error'] = 'Passwords do not match!'
     return render(request, 'auth/register.html', context)
